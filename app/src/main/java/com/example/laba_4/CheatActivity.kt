5352b06b9bda49c69bcaf5cc6af0da2065c59f46
Sasha K <sashakanash@bk.ru>
2025-06-19 13:44:43 +0700

    Добавлен вывод версии API

--- app/src/main/java/com/example/laba_4/CheatActivity.kt
+++ app/src/main/java/com/example/laba_4/CheatActivity.kt
@@ -5,18 +5,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 private const val EXTRA_ANSWER_IS_TRUE ="com.example.laba_4.answers_is_true"
+private const val REQUEST_CODE_CHEAT = 50
+private const val EXTRA_CHEATS_LEFT = "com.example.laba_4.cheats_left"
 
 class CheatActivity : AppCompatActivity() {
 
     private var answerIsTrue = false
     private lateinit var answerTextView:TextView
     private lateinit var showAnswerButton:Button
+    private var cheatsLeft: Int = 0
+    private lateinit var versionAPI:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,19 +35,32 @@ class CheatActivity : AppCompatActivity() {
         answerIsTrue =  intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
         answerTextView = findViewById(R.id.answer_text_view)
         showAnswerButton =  findViewById(R.id.show_answer_button)
+        cheatsLeft = intent.getIntExtra(EXTRA_CHEATS_LEFT, 3)
+        versionAPI = findViewById(R.id.version_API)
+        versionAPI.text = getString(R.string.API, android.os.Build.VERSION.SDK_INT)
         showAnswerButton.setOnClickListener{
-            val answerText = when{
+            var answerText = when{
                 answerIsTrue -> R.string.true_button
                 else -> R.string.false_button
             }
+
+            if (cheatsLeft == 0)
+            {
+                answerText  = R.string.cheat_end
+            }else
+            {
+                Toast.makeText(this, getString(R.string.cheats_left) + (cheatsLeft-1).toString(), Toast.LENGTH_SHORT).show()
+            }
             answerTextView.setText(answerText)
+            setResult(REQUEST_CODE_CHEAT, intent)
         }
     }
 
     companion object{
-        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean, cheatsLeft: Int): Intent {
             return Intent(packageContext, CheatActivity::class.java).apply {
                 putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+                putExtra(EXTRA_CHEATS_LEFT, cheatsLeft)
             }
         }
     }
